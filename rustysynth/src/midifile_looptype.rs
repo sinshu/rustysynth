e3044a5fb90db811825da7326968418f1cdf8fa3
Nobuaki Tanaka <sinshu.d@gmail.com>
2023-05-22 14:47:14 +0900

    Minor tweak.

--- rustysynth/src/midifile_looptype.rs
+++ rustysynth/src/midifile_looptype.rs
@@ -4,12 +4,15 @@
 pub enum MidiFileLoopType {
     /// Specifies the loop start point by a tick value.
     LoopPoint(usize),
+
     /// The RPG Maker style loop.
     /// CC #111 will be the loop start point.
     RpgMaker,
+
     /// The Incredible Machine style loop.
     /// CC #110 and #111 will be the start and end points of the loop.
     IncredibleMachine,
+
     /// The Final Fantasy style loop.
     /// CC #116 and #117 will be the start and end points of the loop.
     FinalFantasy,
