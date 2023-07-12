92aa66db97938660896c07ad9540dd42d04ef92e
Nobuaki Tanaka <sinshu.d@gmail.com>
2023-07-12 19:05:11 +0900

    Minor tweaks.

--- rustysynth/src/array_math.rs
+++ rustysynth/src/array_math.rs
@@ -6,15 +6,15 @@ pub(crate) struct ArrayMath {}
 
 impl ArrayMath {
     pub(crate) fn multiply_add(a: f32, x: &[f32], destination: &mut [f32]) {
-        for i in 0..destination.len() {
-            destination[i] += a * x[i];
+        for (x, destination) in x.iter().zip(destination.iter_mut()) {
+            *destination += a * *x;
         }
     }
 
     pub(crate) fn multiply_add_slope(a: f32, step: f32, x: &[f32], destination: &mut [f32]) {
         let mut a = a;
-        for i in 0..destination.len() {
-            destination[i] += a * x[i];
+        for (x, destination) in x.iter().zip(destination.iter_mut()) {
+            *destination += a * *x;
             a += step;
         }
     }
