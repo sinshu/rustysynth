f95e2cc445ea15c61131f6d3a962304ece27d6b2
Chanjung Kim <freiyer.paxbun@gmail.com>
2023-09-06 14:04:30 +0900

    Restore removed comments in the FourCC parsing logic

--- rustysynth/src/four_cc.rs
+++ rustysynth/src/four_cc.rs
@@ -5,10 +5,11 @@ pub struct FourCC([u8; 4]);
 
 impl FourCC {
     pub(crate) const fn from_bytes(mut bytes: [u8; 4]) -> Self {
-        bytes[0] = replace_if_non_ascii(bytes[0]);
-        bytes[1] = replace_if_non_ascii(bytes[1]);
-        bytes[2] = replace_if_non_ascii(bytes[2]);
-        bytes[3] = replace_if_non_ascii(bytes[3]);
+        // Replace non-ASCII characters with '?'.
+        bytes[0] = replace_with_question_mark_if_non_ascii(bytes[0]);
+        bytes[1] = replace_with_question_mark_if_non_ascii(bytes[1]);
+        bytes[2] = replace_with_question_mark_if_non_ascii(bytes[2]);
+        bytes[3] = replace_with_question_mark_if_non_ascii(bytes[3]);
         Self(bytes)
     }
 
@@ -47,7 +48,7 @@ const fn is_ascii_graphic_or_space(byte: u8) -> bool {
     byte.is_ascii_graphic() || byte == b' '
 }
 
-const fn replace_if_non_ascii(byte: u8) -> u8 {
+const fn replace_with_question_mark_if_non_ascii(byte: u8) -> u8 {
     if is_ascii_graphic_or_space(byte) {
         byte
     } else {
